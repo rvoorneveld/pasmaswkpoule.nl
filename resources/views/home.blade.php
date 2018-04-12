9e73025e73e4d5e791c58e033a44c364a8d2635f
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-04-12 20:51:36 +0200

    added todays games

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,41 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            @if (false === empty($todaysGames))
+                <div class="card">
+                    <div class="card-header">Wedstrijden van de dag</div>
+                    <table class="table table-responsive">
+                        <thead>
+                            <tr>
+                                <th scope="col">Tijd</th>
+                                <th scope="col">Thuis</th>
+                                <th scope="col"></th>
+                                <th scope="col">Uit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($todaysGames as $todaysGame)
+                            <tr>
+                                <th scope="row">{{$todaysGame->time}}</th>
+                                <td>{{$todaysGame->homeCountry->name}}</td>
+                                <td>-</td>
+                                <td>{{$todaysGame->awayCountry->name}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                        You are logged in!
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 </div>
