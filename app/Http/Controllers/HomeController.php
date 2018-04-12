d5625d4a5691ab5dde25e597ddfb97a9555b3d22
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-04-12 20:51:57 +0200

    Parsing todays games to script

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Game;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
     /**
      * Create a new controller instance.
      *
@@ -23,6 +26,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', [
+            'todaysGames' => Game::where('date', Carbon::today()->toDateString())->get()->all(),
+        ]);
     }
+
 }
