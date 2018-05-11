5ed83a75e07a95b80dad725ad865fb6467772a2c
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-05-11 16:55:35 +0200

    Added admin middleware
    Admin middleware checks if the user is of the type administrator

---
+++ app/Http/Middleware/UserBelongsToAdminLevel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class UserBelongsToAdminLevel
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (
+            false === empty($user = Auth::user()) &&
+            20 !== $user->isAdmin
+        ) {
+            return redirect('home');
+        }
+
+        return $next($request);
+    }
+
+}
