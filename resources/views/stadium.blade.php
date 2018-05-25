ea374ed937553e132fb85126efcef8f0cfcf1b89
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-05-25 21:42:46 +0200

    Changed stadiums interface

--- resources/views/stadium.blade.php
+++ resources/views/stadium.blade.php
@@ -1,28 +1,24 @@
 @extends('layouts.app')
-
 @section('content')
-    <h1>Stadions</h1>
     @if (false === empty($stadiums))
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="row">Naam</th>
-                </tr>
-            </thead>
-            <tbody>
+        <h1>Stadions</h1>
+        <div class="row">
             @foreach($stadiums as $stadium)
-                <tr>
-                    <td>
-                        <a href="{{$stadium->url}}" target="_blank">
-                            {{$stadium->name}}
-                        </a>
-                    </td>
-                </tr>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="/images/stadiums/{{ $stadium->image }}" data-holder-rendered="true">
+                        <div class="card-body">
+                            <h5>{{ $stadium->name }}</h5>
+                            <p class="card-text">
+                                {{{ html_entity_decode($stadium->description) }}}
+                            </p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small class="text-muted">{{ $stadium->city->name }}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-            </tbody>
-        </table>
-    @else
-        <p>Er zijn nog geen stadia om weer te geven.</p>
+        </div>
     @endif
-
 @endsection
