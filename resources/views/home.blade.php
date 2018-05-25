915bfb4d22f6a08d1332306e106b96b80be75507
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-05-25 21:42:00 +0200

    Added predictions alert box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 <h1>Welkom {{ Auth::user()->name }}</h1>
+@if (true === $showFillPredictionsBox)
+    <div class="alert alert-warning alert-important" role="alert">
+        Vergeet niet om je <a class="alert-link" href="/games">voorspellingen</a> in te vullen!
+    </div>
+
+@endif
 <div class="row justify-content-center">
     <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
         <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
