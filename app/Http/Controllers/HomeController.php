50e7444578cef6545aad5a4aaed8098809dc076c
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-05-25 21:40:03 +0200

    Added upcomming and predictions check

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Game;
+use App\Predictions;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Feeds;
@@ -29,14 +31,16 @@ class HomeController extends Controller
     {
         return view('home', [
             'todaysGames' => Game::where('date', Carbon::today()->toDateString())->get()->all(),
-            'feed' => $this->getFeedData(),
+            'upcommingGames' => Game::limit(5)->get(),
+            'showFillPredictionsBox' => (Predictions::where('userId', Auth::id())->count() == Game::count()),
+//            'feed' => $this->getFeedData(),
         ]);
     }
 
     protected function getFeedData(): array
     {
         return [
-            'title' => ($feed = Feeds::make('http://www.feedforall.com/sample.xml'))->get_title(),
+            'title' => ($feed = Feeds::make('https://www.voetbalkrant.com/nl/rss/nieuws'))->get_title(),
             'permalink' => $feed->get_permalink(),
             'items' => $feed->get_items(),
         ];
