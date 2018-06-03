75c364a1dd0b4bc0f134883240e1b236cb3de3c4
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-06-03 12:49:32 +0200

    formatting and disable games in past

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -19,6 +19,8 @@
                             <tbody>
                         @foreach ($games[$strPoule] as $game)
                             @php
+                                $date = $carbon->setTimeFromTimeString("{$game->date} {$game->time}");
+                                $disabled = false === $date->isFuture() ? ' disabled' : '';
                                 $gamePrediction = $userPredictions[$game->id] ?? false;
                                 $goalsHome = $gamePrediction['goalsHome'] ?? '';
                                 $goalsAway = $gamePrediction['goalsAway'] ?? '';
@@ -27,33 +29,59 @@
                             @endphp
                             <tr>
                                 <td>
-                                    @if (null !== $intPoints = $gamePrediction['points'])
-                                        {{ $intPoints }}
-                                        @else
-                                    {{{ ($date = $carbon->setTimeFromTimeString("{$game->date} {$game->time}"))->format('d') }}}<br />
+                                    {{{ $date->format('d') }}}<br />
                                     {{ $date->format('M') }}
-                                        @endif
                                 </td>
                                 <td>
                                     <img class="flag" src="/images/flags/{{$game->homeCountry->flag}}" /><br />
                                     {{ html_entity_decode($game->homeCountry->name) }}
                                 </td>
                                 <td width="60">
-                                    <input maxlength="1" style="max-width:50px; text-align:center; font-size:18px;" name="{{$game->id}}[goalsHome]" type="text" class="form-control" value="{{ $goalsHome }}">
+                                    <input
+                                        maxlength="1"
+                                        style="max-width:50px; text-align:center; font-size:18px;"
+                                        name="{{$game->id}}[goalsHome]"
+                                        type="text"
+                                        class="form-control"
+                                        value="{{ $goalsHome }}"{{ $disabled }}
+                                    >
                                 </td>
                                 <td width="25">-</td>
                                 <td width="60">
-                                    <input maxlength="1" style="max-width:50px; text-align:center; font-size:18px;" name="{{$game->id}}[goalsAway]" type="text" class="form-control" value="{{ $goalsAway }}">
+                                    <input maxlength="1"
+                                        style="max-width:50px; text-align:center; font-size:18px;"
+                                        name="{{$game->id}}[goalsAway]"
+                                        type="text"
+                                        class="form-control"
+                                        value="{{ $goalsAway }}"
+                                        {{ $disabled }}
+                                    >
                                 </td>
                                 <td>
                                     <img class="flag" src="/images/flags/{{$game->awayCountry->flag}}" /><br />
                                     {{ html_entity_decode($game->awayCountry->name) }}
                                 </td>
                                 <td width="60">
-                                    <input maxlength="1" style="max-width:50px; text-align:center; font-size:18px; background: yellow;" name="{{$game->id}}[cardsYellow]" type="text" class="form-control" value="{{ $cardsYellow }}">
+                                    <input
+                                        maxlength="1"
+                                        style="max-width:50px; text-align:center; font-size:18px; background: yellow;"
+                                        name="{{$game->id}}[cardsYellow]"
+                                        type="text"
+                                        class="form-control"
+                                        value="{{ $cardsYellow }}"
+                                        {{ $disabled }}
+                                    >
                                 </td>
                                 <td width="60">
-                                    <input maxlength="1" style="max-width:50px; text-align:center; font-size:18px; background: red;" name="{{$game->id}}[cardsRed]" type="text" class="form-control" value="{{ $cardsRed }}">
+                                    <input
+                                        maxlength="1"
+                                        style="max-width:50px; text-align:center; font-size:18px; background: red;"
+                                        name="{{$game->id}}[cardsRed]"
+                                        type="text"
+                                        class="form-control"
+                                        value="{{ $cardsRed }}"
+                                        {{ $disabled }}
+                                    >
                                 </td>
                             </tr>
                         @endforeach
