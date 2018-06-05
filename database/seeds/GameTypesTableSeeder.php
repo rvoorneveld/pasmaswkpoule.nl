41e6c605ace9657eadc8ee5bcf4ae1e8ebe1d569
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-06-05 21:30:52 +0200

    refactored db table gametypes to game_types

--- database/seeds/GameTypesTableSeeder.php
+++ database/seeds/GameTypesTableSeeder.php
@@ -21,7 +21,7 @@ class GameTypesTableSeeder extends Seeder
             '3',
             'FINALE'
         ] as $name) {
-            DB::table('gameTypes')->insert([
+            DB::table('game_types')->insert([
                 'name' => $name,
             ]);
         }
