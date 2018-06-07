e999b06275da0dab54da302a9c491db1ed0f6303
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-06-07 10:29:32 +0200

    Updated composer for Debugbar

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Awjudd\FeedReader\FeedReaderServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -189,6 +190,7 @@ return [
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
         'DB' => Illuminate\Support\Facades\DB::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
         'File' => Illuminate\Support\Facades\File::class,
