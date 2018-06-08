090282002f6b5414d41729625f58363e872009e8
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-06-08 16:24:49 +0200

    Removed profile link from menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,9 +66,9 @@
                                 </a>
 
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="/profile">
-                                        Profiel
-                                    </a>
+                                    {{--<a class="dropdown-item" href="/profile">--}}
+                                        {{--Profiel--}}
+                                    {{--</a>--}}
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
