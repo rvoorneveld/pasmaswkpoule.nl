f7fe7413e2b504d1796231e0fd7686724068c45a
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-06-10 15:37:51 +0200

    Added rules page

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -2,10 +2,20 @@
 
 @section('content')
     <h1>Spelregels</h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus erat arcu, dictum nec leo eu, pharetra ultrices ante. Ut in lectus suscipit, condimentum leo non, semper sem. Etiam condimentum, erat at vulputate cursus, turpis sapien laoreet felis, vitae pretium odio tortor quis tellus. In vitae dolor ut lorem auctor volutpat sit amet ullamcorper lorem. Pellentesque dignissim fringilla ligula. Cras efficitur rutrum diam, sit amet molestie metus iaculis eget. Aliquam eget orci leo. Morbi nec molestie orci. Integer semper tristique ex in mattis. Integer pulvinar commodo ex ac dignissim.
-    </p>
-    <p>
-        Vivamus egestas dui arcu, quis ullamcorper massa aliquet id. Donec eleifend ipsum sit amet dictum sodales. Pellentesque ante felis, porttitor non imperdiet vel, molestie sit amet eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In venenatis et mi ac hendrerit. Donec iaculis aliquam neque eu vehicula. Ut efficitur a eros non hendrerit. Quisque porttitor vestibulum ultricies. Donec fermentum ex elit, in ultricies lectus mattis dapibus. Donec scelerisque, felis id bibendum rhoncus, enim sem laoreet orci, id mollis sapien enim ut arcu. Fusce in sollicitudin nibh.
-    </p>
+
+    <h2>Puntentelling:</h2>
+    <ul>
+        <li>Volledige uitslag correct: 5 punten</li>
+        <li>Uitslag thuis- of uitploeg correct: 2 punten</li>
+        <li>Zowel rode als gele kaarten correct: 5 punten</li>
+        <li>Rode - of gele kaarten correct: 2 punten</li>
+    </ul>
+
+    <h2>Kosten</h2>
+    <p>Wij vragen voor iedere deelname &euro; 5,-. Dit bedragt dient cash betaald te worden aan Jordi of Rick.
+        Zodra je betaling binnen is doe je daadwerkelijk mee voor de prijzen.</p>
+
+    <h2>Prijzen</h2>
+    <p>Van het inschrijfgeld zullen er prijzen worden gekocht. De nummers 1, 2, 3 evenals de nummer laatst vallen in de prijzen.
+        Maar ook ergens midden in de ranglijst zullen er prijzen worden verdeeld. Houd de ranglijst dus goed in de gaten.</p>
 @endsection
