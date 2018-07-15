8e86afd24d39850dd752dbb4450b1e9a68721aa2
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-07-15 13:52:42 +0200

    Tekstual update

--- resources/views/emails/payment-reminder.blade.php
+++ resources/views/emails/payment-reminder.blade.php
@@ -4,7 +4,7 @@
 Uit onze administratie blijkt dat je nog niet hebt betaald voor deelname aan de WK Poule.
 Wij verzoeken je per ommegaande om de betaling in orde te maken.
 
-De betaling kan overgemaakt worden naar: NL28 RABO 0324 6727 13 t.n.v J.R. Pasma.
+De betaling van &euro; 5,- kan overgemaakt worden naar: NL28 RABO 0324 6727 13 t.n.v J.R. Pasma.
 
 Sportieve groet,<br>
 {{ config('app.name') }}
