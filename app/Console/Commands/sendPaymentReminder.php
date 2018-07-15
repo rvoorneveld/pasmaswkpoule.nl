12a5afc08d879a78aa2af52e314b7bf08a62bc36
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-07-15 13:52:57 +0200

    Bugfix to get correct mail script

--- app/Console/Commands/sendPaymentReminder.php
+++ app/Console/Commands/sendPaymentReminder.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Mail\RulesInKnockOutPhase;
+use App\Mail\PaymentReminder;
 use App\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
